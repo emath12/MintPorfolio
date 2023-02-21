b0db2a3013bd27b80b78f3f17e1740e94946a5b7
Ethan <ethan.mathieu@yale.edu>
2023-02-21 15:10:42 -0500

    basic basic react setup

---
+++ finance_app/src/components/Root.js
@@ -0,0 +1,19 @@
+import './Root.css';
+import {useNavigate} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Home.js';
+
+
+
+
+function Root() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Root;
