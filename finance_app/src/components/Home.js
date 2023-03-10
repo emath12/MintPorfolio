83d886e9acafbf653f09654c110dc2aba94ee8a7
Ethan <ethan.mathieu@yale.edu>
2023-03-09 22:47:59 -0500

    basic MVP

--- finance_app/src/components/Home.js
+++ finance_app/src/components/Home.js
@@ -1,12 +1,17 @@
 import './Home.css';
-import React from "react";
+import React from "react"
+import Graph from './Graph.js';
+;
 
 function Home() {
     return (
-        <div className="Title">
-            <h1>
-                Portfolio Returns
-            </h1>
+        <div className="Home">
+            <div className="Title">
+                <h1>
+                    Portfolio Returns
+                </h1>
+            </div>
+            <Graph/>
         </div>
         
     );
