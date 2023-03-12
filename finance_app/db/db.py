0aaa19be73f14bfceab3bd4aee89209e4d2a7580
Taimur Moolji <Taimur Moolji>
2023-03-12 14:42:38 -0400

    changed server routing for portfolio data and pulled market data + serialized

--- finance_app/db/db.py
+++ finance_app/db/db.py
@@ -7,8 +7,11 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/dataframe')
-def call_api():
+init_pvalue = 1
+
+
+@app.route('/pf_dataframe')
+def call_pf():
     aapl = yf.Ticker("AAPL")
     msft = yf.Ticker("MSFT")
     amzn = yf.Ticker("AMZN")
@@ -27,9 +30,31 @@ def call_api():
     pf["Dates"] = df.index.strftime("%Y-%m-%d").tolist()  # Convert Timestamps to strings
     pf["Vals"] = df.sum(axis=1).tolist()
 
+    init_pvalue = pf["Dates"][0]
+
     j_string = json.dumps(pf.to_dict(orient='list'))
 
     return j_string
+
+
+@app.route('/market_dataframe')
+def call_market():
+    mkt = yf.Ticker("^GSPC")
+    df = pd.DataFrame()
+    mkt_hist = mkt.history(start="2023-01-01")["Close"]
+
+    df["mkt"] = (init_pval / mkt_hist[0]) * mkt_hist
+
+    pf = pd.DataFrame()
+    pf["Dates"] = df.index.strftime("%Y-%m-%d").tolist()  # Convert Timestamps to strings
+    pf["Vals"] = df.sum(axis=1).tolist()
+
+    init_pvalue = pf["Dates"][0]
+
+    j_string = json.dumps(pf.to_dict(orient='list'))
+
+    return j_string
+
 
 
 if __name__ == '__main__':
