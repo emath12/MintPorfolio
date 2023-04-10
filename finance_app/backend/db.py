b16d6f50f06c7d77664cab09635bf81b4fb35efe
Taimur Moolji <Taimur Moolji>
2023-04-10 10:15:18 -0400

    Changed relational mappings

--- finance_app/backend/db.py
+++ finance_app/backend/db.py
@@ -1,23 +1,29 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
 
-class Portfolio(Base):
-    __tablename__ = 'portfolio'
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    date = Column(String)
-
-
 class User(Base):
-    __tablename__ = 'user'
-    username = Column(String, primary_key=True)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
     password = Column(String)
+    portfolios = relationship("Portfolio", back_populates="user")
+
+class Portfolio(Base):
+    __tablename__ = 'portfolios'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="portfolios")
+    date = Column(Date)
+    positions = relationship("Position", back_populates="portfolio")
 
 class Position(Base):
-    __tablename__ = 'position'
-    port_id = Column(Integer, primary_key=True)
-    ticker = Column(String, primary_key=True)
-    quantity = Column(Integer)
+    __tablename__ = 'positions'
+    id = Column(Integer, primary_key=True)
+    portfolio_id = Column(Integer, ForeignKey('portfolios.id'))
+    portfolio = relationship("Portfolio", back_populates="positions")
+    ticker = Column(String)
+    amount = Column(Float)
