5bad9f98abf9f6aa5b7435c60dc748d9eb148692
Taimur Moolji <Taimur Moolji>
2023-04-10 09:49:04 -0400

    added db model to db.py

--- finance_app/backend/db.py
+++ finance_app/backend/db.py
@@ -1,2 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class Portfolio(Base):
+    __tablename__ = 'portfolio'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    date = Column(String)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    username = Column(String, primary_key=True)
+    password = Column(String)
+
+class Position(Base):
+    __tablename__ = 'position'
+    port_id = Column(Integer, primary_key=True)
+    ticker = Column(String, primary_key=True)
+    quantity = Column(Integer)
