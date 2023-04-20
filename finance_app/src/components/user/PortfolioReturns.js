82c54f6c07825458100a0e3fc3a0a56554b714d7
Daphne Raskin <daphneraskin@gmail.com>
2023-04-20 19:25:37 -0400

    Set background + Footer on Portfolio Returns Page

--- finance_app/src/components/user/PortfolioReturns.js
+++ finance_app/src/components/user/PortfolioReturns.js
@@ -2,18 +2,22 @@ import './PortfolioReturns.css';
 import React from "react"
 import Graph from './Graph.js';
 import Header from './Header.js';
+import Footer from './Footer.js'
 
 function PortfolioReturns() {
     return (
         <>
-            <Header />
-            <div className="Home">
-                <div className="Title">
-                    <h1>
-                        Portfolio Returns
-                    </h1>
+            <div className='ReturnsPage'>
+                <Header />
+                <div className="Home">
+                    <div className="Title">
+                        <h1>
+                            Portfolio Returns
+                        </h1>
+                    </div>
+                    <Graph />
                 </div>
-                <Graph />
+                <Footer></Footer>
             </div>
         </>
     );
